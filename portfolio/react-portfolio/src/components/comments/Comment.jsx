6461de4265f3e188892c1f686a9f7a2ac4f2fcb9
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-01 10:41:21 -0300

    create-react-portfolio && basic setup config && index.css

---
+++ portfolio/react-portfolio/src/components/comments/Comment.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './comment.css';
+
+const Comment = () => {
+  return (
+    <div>Comment</div>
+  )
+}
+
+export default Comment;
---
+++ portfolio/react-portfolio/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container"></div>
+    </header>
+  )
+}
+
+export default Header;
