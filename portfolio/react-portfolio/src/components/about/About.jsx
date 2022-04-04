096d11fdcea3991b4678cbeb2c5d2de961a28a57
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-04 10:19:09 -0300

    ajustes about.css

--- portfolio/react-portfolio/src/components/about/About.jsx
+++ portfolio/react-portfolio/src/components/about/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import MeWithIt from '../../assets/fatnando_it.png';
+import { HiOutlineAcademicCap } from 'react-icons/hi';
+import { RiFolderChartFill } from 'react-icons/ri';
+import { SiGithubactions } from 'react-icons/si';
+import { GiSharpSmile } from 'react-icons/gi';
 import './about.css';
 
 const About = () => {
@@ -7,19 +11,40 @@ const About = () => {
     <section id="about">
       <h5>A little</h5>
       <h2>About Myself</h2>
+
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-img">
             <img src={ MeWithIt } alt="Man badguy with It the clown" />            
           </div> 
         </div>
-      </div>
-      <div className="about__content">
-        <div className="about__cards">
-          <article className="about__card">
-            <h5>Experience</h5>
-            <small></small>
-          </article>
+        
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <HiOutlineAcademicCap className="about__card-icon" />
+              <h5>Experience</h5>
+              <small>Trybe student (july 2022)</small>
+            </article>
+
+            <article className="about__card">
+              <RiFolderChartFill className="about__card-icon" />
+              <h5>Projects</h5>
+                <img src={ SiGithubactions } alt="GitHub logo" />
+                <a 
+                  href="https://github.com/fernandosenacruz?tab=repositories"
+                  className="btn btn-primary"
+                >
+                </a>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio veritatis eos molestiae itaque, natus delectus sint eius et deleniti tempore magnam voluptatum officiis quam, quisquam omnis sapiente numquam voluptatem perspiciatis, eaque optio laboriosam. Hic asperiores ex veniam animi odit illo. Aliquid, mollitia nisi voluptatem nemo quasi pariatur debitis sunt officiis!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Lest's talk { GiSharpSmile }</a>
+
         </div>
       </div>
     </section>
