219eb4fe87aa2b84b1c83219ef4ebce6ffe8c2d8
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-05 10:20:15 -0300

    experience componnent done

--- portfolio/react-portfolio/src/components/experience/Experience.jsx
+++ portfolio/react-portfolio/src/components/experience/Experience.jsx
@@ -1,10 +1,80 @@
 import React from 'react';
+import { FaCheckSquare } from 'react-icons/fa';
+import coachDepression from '../../assets/coach_depression.gif';
 import './experience.css';
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>Some Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__itens">
+          <h3>Fullsatck Developement</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>HTML CSS JS</h4>
+                <small>intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>React Redux/Context</h4>
+                <small>intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4 className="text-light">JestS</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap/MUI</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>MYSQL</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Node.JS</h4>
+                <small>intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>TypeScript</h4>
+                <small>intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__itens">
+          <h3>Coach Depression</h3>
+          <div>
+            <a href="https://coach-da-depressao-front.vercel.app/">
+              <img src={coachDepression} alt="git deploy coach depression" className="experience__details-img"/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Experience;
