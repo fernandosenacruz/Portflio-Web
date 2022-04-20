3e84112fd088894f0854a03ebf75d04f7feec66a
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-20 18:04:54 -0300

    0

--- portfolio/react-portfolio/src/components/comments/Comment.jsx
+++ portfolio/react-portfolio/src/components/comments/Comment.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import API from '../../services/API';
 import './comment.css';
 
@@ -21,8 +21,43 @@ const Comment = () => {
     getComments();
   }, []);
 
+  const form = useRef();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const comment = form.comment.value;
+    const author = form.author.value;
+
+    try {
+      await API.post('/comment', {
+        title,
+        comment,
+        author,
+      },
+      console.log({ title, comment, author }));
+      
+      setComments([...comments, {
+        title,
+        comment,
+        author,
+      }]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <section id="comment">
+      <form ref={ form }>
+        <input type="text" name="title" placeholder="Title" />
+        <imput type="text" name="comment" placeholder="Comment" />
+        <input type="text" name="author" placeholder="Author" />
+        <button type="submit" className="btn btn-primary" onSubmit={ handleSubmit }>Add Comment</button>
+      </form>
+
       <div className="container comments__container">
         <h3>{comments.map((comment) => comment.title)}</h3>
         <h4>{comments.map((comment) => comment.comment)}</h4>
