b37ba058c4ede99abd9d57c23a03ec2d22f95c9b
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-20 15:39:03 -0300

    Comment.jsx render comments

--- portfolio/react-portfolio/src/components/comments/Comment.jsx
+++ portfolio/react-portfolio/src/components/comments/Comment.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../../services/API';
 import './comment.css';
 
 const Comment = () => {
+  const [comments, setComments] = useState([{
+    title: 'No comments yet',
+    comment: '',
+    author: null
+  }]);
+
+  useEffect(() => {
+    async function getComments() {
+      try {
+        const { data } = await API.get('/comments');
+        setComments(data);        
+      } catch (error) {
+        console.log(error);        
+      }
+    }
+    getComments();
+  }, []);
+
   return (
-    <section id="comment">Comment</section>
+    <section id="comment">
+      <div className="container comments__container">
+        <h3>{comments.map((comment) => comment.title)}</h3>
+        <h4>{comments.map((comment) => comment.comment)}</h4>
+        <h5>{comments.map((comment) => comment.author)}</h5>
+      </div>
+    </section>
   )
 }
 
---
+++ portfolio/react-portfolio/src/services/API.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const DATABASE_URL =
+  process.env.NODE_ENV === 'production'
+  ? 'http://localhost:3000'
+  : 'https://fatnando-portfoli-backend.herokuapp.com';
+
+    const API = axios.create({ baseURL : DATABASE_URL });
+
+export default API;
