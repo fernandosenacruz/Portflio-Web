1875c4c46bc0f23af3419488fa7b5854a2a7ba80
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-20 21:01:15 -0300

    cors config

--- portfolio/portfolio-backend/src/app.ts
+++ portfolio/portfolio-backend/src/app.ts
@@ -5,18 +5,19 @@ import { router } from "./router";
 export class App{
   public server: express.Application;
 
-  public cors: cors.CorsOptions = {
-    origin: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  };
+  // public cors: cors.CorsOptions = {
+  //   origin: true,
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   credentials: true
+  // };
 
   constructor(){
     this.server = express();
     this.middleware();
     this.router();
+    this.config();
   }
 
   private middleware(){
@@ -25,5 +26,15 @@ export class App{
 
   private router(){
     this.server.use(router);
+  }
+
+  private config(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+    this.server.use(accessControl);
   }
 }
