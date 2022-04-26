16611991b356e77f31ab82d37375249cfdb6926e
Fernando Sena <fernandosenacruz@gmail.com>
2022-04-26 09:04:34 -0300

    process.env updated

--- portfolio/react-portfolio/src/services/API.js
+++ portfolio/react-portfolio/src/services/API.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const DATABASE_URL =
-  process.env.NODE_ENV === 'https://fatnando-portfoli-backend.herokuapp.com'
-  // 'Production'
-  // ? 'http://localhost:3000'
-  // : 'https://fatnando-portfoli-backend.herokuapp.com';
+  process.env.NODE_ENV === 
+  'Production'
+  ? 'http://localhost:3000'
+  : 'https://fatnando-portfoli-backend.herokuapp.com';
 
     const API = axios.create({ baseURL : DATABASE_URL });
 
